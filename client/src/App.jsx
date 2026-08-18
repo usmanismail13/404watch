bfe6e43f5f6379ba02d9926d390f5cc2e2947c3a
Usman Ismail <usmanismailalpha69@gmail.com>
2026-08-18 11:47:18 +0500

    Create 404 errors page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Pricing from "./pages/Pricing";
 import Dashboard from "./pages/Dashboard";
 import Website from "./pages/Website";
+import Errors404 from "./pages/Errors404";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/website" element={<Website />} />
+        <Route path="/errors-404" element={<Errors404 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Errors404.jsx
@@ -0,0 +1,10 @@
+function Errors404() {
+  return (
+    <div>
+      <h1>404 Errors</h1>
+      <p>Monitor and manage your website's broken links.</p>
+    </div>
+  );
+}
+
+export default Errors404;
