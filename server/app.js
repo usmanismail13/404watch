23d73780e78caeef5b4dcaa43467020ec3ac8ec4
Usman Ismail <usmanismailalpha69@gmail.com>
2026-08-20 17:23:23 +0500

    Create health check endpoint

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 module.exports = app;
