044916a858d9fb4b5b5c88f15527e3e080286b76
Usman Ismail <usmanismailalpha69@gmail.com>
2026-08-25 16:39:37 +0500

    Add supported URL scheme filtering

--- worker/services/linkExtractor.js
+++ worker/services/linkExtractor.js
@@ -1,5 +1,18 @@
 const cheerio = require("cheerio");
 
+function isSupportedUrl(url) {
+  try {
+    const parsedUrl = new URL(url);
+
+    return (
+      parsedUrl.protocol === "http:" ||
+      parsedUrl.protocol === "https:"
+    );
+  } catch (error) {
+    return false;
+  }
+}
+
 function extractLinks(html, baseUrl) {
   const $ = cheerio.load(html);
   const links = [];
@@ -13,6 +26,11 @@ function extractLinks(html, baseUrl) {
 
     try {
       const absoluteUrl = new URL(href, baseUrl).href;
+
+      if (!isSupportedUrl(absoluteUrl)) {
+        return;
+      }
+
       links.push(absoluteUrl);
     } catch (error) {
       // Ignore invalid URLs
